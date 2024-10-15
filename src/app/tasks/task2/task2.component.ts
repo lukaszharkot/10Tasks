d470b8452820615820abd93ca8b2df2137c28d13
lukaszharkot <lukasz.harkot22@gmail.com>
2024-10-15 17:44:37 +0200

    animations and visual changes

--- src/app/tasks/task2/task2.component.ts
+++ src/app/tasks/task2/task2.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-task2',
   templateUrl: './task2.component.html',
-  styleUrls: ['./task2.component.css']
+  styleUrls: ['./task2.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('400ms', style({ opacity: 1 }))
+      ]),
+    ]),
+  ]
 })
 export class Task2Component implements OnInit {
 
--- src/app/tasks/tasklist/tasklist.component.ts
+++ src/app/tasks/tasklist/tasklist.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { TaskService } from '../../services/task.service';
+import { trigger, style, animate, transition, query, stagger } from '@angular/animations'; 
 
 @Component({
   selector: 'app-tasklist',
   templateUrl: './tasklist.component.html',
-  styleUrls: ['./tasklist.component.css']
+  styleUrls: ['./tasklist.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', stagger('200ms', [
+          animate('300ms ease-out', style({ opacity: 1 }))
+        ]), { optional: true })
+      ])
+    ])
+  ]
 })
 export class TasklistComponent implements OnInit {
   tasks: any[] = [];
+  taskMessage = 'View task >'
 
   taskService = inject(TaskService);
+
+  showMessage() {
+    this.taskMessage = "It's above you!";
+  }
 
   ngOnInit(): void {
     this.tasks = this.taskService.getTasks();
