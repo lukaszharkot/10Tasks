d92c5c36470fcb444b977540ecf5c3057662037a
lukaszharkot <lukasz.harkot22@gmail.com>
2024-10-19 01:22:23 +0200

    New CanDeactivate() interface

---
+++ src/app/interfaces/can-it-deactivate.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface CanItDeactivate {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class CanDeactivateGuard implements CanDeactivate<CanItDeactivate> {
+  canDeactivate(
+    component: CanItDeactivate
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return component.canDeactivate ? component.canDeactivate() : true;
+  }
+}
